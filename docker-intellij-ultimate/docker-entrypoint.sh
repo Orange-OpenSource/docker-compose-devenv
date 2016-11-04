0e74b5b65c7591e4c44399744c2d57b56a747346
mgodlewski <mathieu.godlewski@orange.com>
2016-11-04 12:15:37 +0100

    fix: no more bash_aliases

--- docker-intellij-ultimate/docker-entrypoint.sh
+++ docker-intellij-ultimate/docker-entrypoint.sh
@@ -2,7 +2,6 @@
 set -e
 
 if [ "$1" = 'idea' ]; then
-	source /home/developer/.bash_aliases
 	sudo chown -R developer:developer /home/developer/
 	# for idea update
 	sudo chown -R developer:developer /opt/
