fb2e856acab541b9bd5f909fc2282b67affd9a9e
rremoissonnet <rudy.remoissonnet@orange.com>
2020-12-29 16:35:15 +0100

    add requirement for moving to Orange open source repo

--- sample-java-8/ide/entrypoint.sh
+++ sample-java-8/ide/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Software Name : docker-compose-devenv
+# Version: 1.0
+# SPDX-FileCopyrightText: Copyright (c) 2020 Orange OBS SA
+# SPDX-License-Identifier: MIT
+#
+# This software is distributed under the MIT License,
+# the text of which is available at https://spdx.org/licenses/MIT.html
+# or see the "license.txt" file for more details.
+#
+# Author: rudy remoissonnet rudy.remoissonnet@orange.com
 
 sudo chown developer.developer \
      /home/developer/.m2 \
